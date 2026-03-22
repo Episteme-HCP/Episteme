762461f0798cef555ed34ae29c859ad729eac492
Silvere Martin-Michiellot <silvere@example.com>
2026-03-22 23:31:20 +0100

    Force sync NBody backends with latest fixes

---
+++ run_benchmark_vm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export JAVA_HOME=/home/silve/jdk-25.0.1
+export PATH=$JAVA_HOME/bin:/usr/bin:/bin:$PATH
+cd ~/Episteme
+git reset --hard origin/main
+git pull
+mvn clean compile -pl episteme-benchmarks -am -DskipTests
+mvn exec:java -pl episteme-benchmarks -Dexec.mainClass="org.episteme.benchmarks.SystematicBenchmark" -Dexec.args="--run-all --domain Physics --iterations 1" -Pjcuda-linux-x86_64
